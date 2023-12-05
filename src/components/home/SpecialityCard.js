68a44d24670c3a4bc5be76235201244680f6343d
Rayen-Allaya <allayarayen@gmail.com>
2023-12-05 11:18:52 +0100

    changing common folders

--- src/components/home/SpecialityCard.js
+++ src/components/home/SpecialityCard.js
@@ -1,29 +1,26 @@
-import Card from '../common/Card';
-import React from 'react';
-import { View, Image,Text, StyleSheet } from 'react-native';
+import Card from "../common/basic/Card";
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
 
-const SpecialityCard = ({color,imageSource}) => {
+const SpecialityCard = ({ color, imageSource }) => {
   return (
-    <Card style={[styles.container, {backgroundColor: color}]}>
-      <Image
-        source={imageSource}
-        style={styles.logo}
-      />
+    <Card style={[styles.container, { backgroundColor: color }]}>
+      <Image source={imageSource} style={styles.logo} />
     </Card>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent:'center',
-    display: 'flex',
-    alignItems: 'center',
-    width:130,
-    height:130
+    justifyContent: "center",
+    display: "flex",
+    alignItems: "center",
+    width: 130,
+    height: 130,
   },
   logo: {
-    resizeMode : "contain",
-    height : 100,
+    resizeMode: "contain",
+    height: 100,
   },
 });
 
